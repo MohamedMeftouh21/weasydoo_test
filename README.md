ea174737cd40ebf870be1d993a36bf46cec23d71
MohamedMeftouh21 <moaddameftouh@gmail.com>
2024-04-20 12:03:24 +0100

    add (Tâche 5 : Gestion des Niveaux de Récompense)

---
+++ README.md
@@ -0,0 +1,37 @@
+# Contact Travel
+## Contexte
+
+Contact Travel est un module Odoo destiné à la gestion des voyages pour les contacts.
+
+## Installation et configuration 
+Bienvenue dans ce dépôt ! Ici, vous trouverez le code source du test disponible à l'adresse suivante : [Contact Travel](https://github.com/MohamedMeftouh21/contact_travel/tree/main/addons/contact_travel).
+
+
+Dans ce test, j'ai configuré un environnement Docker pour exécuter l'application (module) Odoo version 16. Vous trouverez ici le code source ainsi que les fichiers de configuration nécessaires.
+### Testé avec :
+   - Odoo 16.0 (docker image)
+   - PostgreSQL 13 (docker image)
+   - Docker version 24.0.5  (Macos intel)
+  
+
+
+Pour démarrer l'application Odoo à l'aide de Docker, suivez ces étapes :
+
+1. Assurez-vous d'avoir [Docker](https://docs.docker.com/get-docker/) et 
+[Docker-Compose](https://docs.docker.com/compose/install/) installés sur votre système.
+2. Clonez ce dépôt sur votre machine.
+3. Accédez au répertoire du projet.
+4. Utilisez la commande suivante pour démarrer les conteneurs Docker :
+   ```bash
+   docker-compose up -d
+
+
+
+____________
+
+
+### Recommandation
+
+Utiliser la technique [multi-stage](https://medium.com/@pranaymate/multi-stage-docker-build-streamlining-your-python-app-deployment-a8185601666f)  est recommandé pour réduire la taille des images Docker, ce qui peut améliorer les performances de déploiement et réduire la consommation de ressources système.
+
+____
--- addons/contact_travel/models/res_partner.py
+++ addons/contact_travel/models/res_partner.py
@@ -1,6 +1,5 @@
 from odoo import models, fields, api, _
 import logging as log
-from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
@@ -8,11 +7,18 @@ class ResPartner(models.Model):
 
     voyage_ids = fields.One2many('voyage', 'partner_id', string='Voyages')
 
-    voyage_count = fields.Integer( string='Voyages', compute='_compute_voyage_count')
+    voyage_count = fields.Integer( string='Voyages', compute='_compute_voyage_count') #
+    #depense_totale = fields.Float(string="Dépense Totale",compute="_compute_total_depense")#
 
-  
+    recompense_levels = fields.Selection(
+    [('argent', 'Argent'), ('or', 'Or'), ('platine', 'Platine')],
+    string='Niveau de récompense',
+    default='argent',
+    required=True,
+    compute="_compute_recompense_levels" ,readonly=False,
+    store=True
 
-
+)
 
     """
     Cette fonction permet d'afficher la liste des voyages liés au contact lorsqu'on clique sur le bouton intelligent
@@ -36,5 +42,22 @@ class ResPartner(models.Model):
             partner.voyage_count = len(partner.voyage_ids)
 
 
-    
-   
+
+
+
+
+    """
+    Cette fonction met à jour automatiquement le niveau de récompense du client,
+    en fonction de ses dépenses de voyage.
+    """
+    @api.depends('recompense_levels','voyage_ids')
+    def _compute_recompense_levels(self): 
+
+       for record in self:
+            total_amount = sum(voyage.montant for voyage in record.voyage_ids) #Calculer les dépenses totales de voyages du contact (utilisateur) pour définir son niveau de récompense.
+            if total_amount < 50000 :
+                record.recompense_levels = 'argent'
+            elif total_amount  >= 50000 and total_amount  < 100000 :
+                record.recompense_levels = 'or'
+            elif total_amount >= 100000 :
+                record.recompense_levels = 'platine'
